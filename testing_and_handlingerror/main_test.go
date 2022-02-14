75be76d3d8707a52985de0674e48a0510c27d354
akthakur <akthakur@informatica.com>
2022-02-14 15:27:56 +0530

    added new files

--- testing_and_handlingerror/main_test.go
+++ testing_and_handlingerror/main_test.go
@@ -2,18 +2,48 @@ package main
 
 import "testing"
 
-func TestDivideNumbers(t *testing.T) {
-	_, err := DivideNumbers(100.0, 10.0)
-	if err != nil {
-		t.Error("Got an error when we should not")
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isErr    bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0, 0.0, true},
+}
+
+func TestDivisor(t *testing.T) {
+	for _, item := range tests {
+		if !item.isErr {
+			_, err := DivideNumbers(item.dividend, item.divisor)
+			if err != nil {
+				t.Error("Got an error when we should not")
+			}
+		} else {
+			_, err := DivideNumbers(item.dividend, item.divisor)
+			if err == nil {
+				t.Error("Dis not got error when we should")
+			}
+
+		}
+
 	}
 
 }
 
-func TestBadDivide(t *testing.T) {
-	_, error := DivideNumbers(100.0, 0)
-	if error == nil {
-		t.Error("Dis not get an error when we should have")
-	}
-
-}
+//func TestDivideNumbers(t *testing.T) {
+//	_, err := DivideNumbers(100.0, 10.0)
+//	if err != nil {
+//		t.Error("Got an error when we should not")
+//	}
+//
+//}
+//
+//func TestBadDivide(t *testing.T) {
+//	_, error := DivideNumbers(100.0, 0)
+//	if error == nil {
+//		t.Error(" not getting an error when we should have")
+//	}
+//
+//}
